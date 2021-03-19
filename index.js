47dd58912ed24a69f188ad467f64ade796d901dd
Josh Levan <joshlevan@Joshs-iMac-2.local>
2021-03-19 13:04:52 -0400

    task 5 and 6

--- index.js
+++ index.js
@@ -243,8 +243,9 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s( /*Your Code Here*/ ) {
-    /*Your Code Here*/
+
+function get20s(arr) {
+
 }
 
 
@@ -258,10 +259,10 @@ function get20s( /*Your Code Here*/ ) {
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist( /*Your Code Here*/ ) {
-    /*Your Code Here*/
+function removeArtist(arr, index) {
+    arr.splice(index, 1);
+    return arr.length;
 }
-
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
@@ -279,8 +280,16 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist( /*Your Code Here*/ ) {
-    /*Your Code Here*/
+function addArtist(arr) {
+    arr.push({
+        id: 20,
+        name: 'Josh Levan',
+        years: '1985 - current day',
+        genre: 'Web Design',
+        nationality: 'American',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut laoreet ornare magna, nec hendrerit urna tincidunt in. Donec eleifend iaculis augue, non consequat mi consectetur vel.'
+    })
+    return arr;
 }
 
 
